491e96f78a4a5dc28cc8605372b2267a558b3b99
juan1m3 <juanperez14oc@gmail.com>
2022-06-19 23:46:59 -0500

    spa init

---
+++ app/App.js
@@ -0,0 +1,6 @@
+export function App(){
+    const root = document.getElementById('root');
+    const h1 = document.createElement('h1');
+    h1.textContent = "Bienvenido a SPA";
+    root.appendChild(h1);
+}
