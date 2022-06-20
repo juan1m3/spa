9c83bb5d7b3c4fa8f3a9b1ac8bec4635081eb5eb
juanperez14oc@gmail.com <juanperez14oc@gmail.com>
2022-06-20 00:28:49 -0500

    h2 add

--- app/App.js
+++ app/App.js
@@ -1,6 +1,11 @@
 export function App(){
     const root = document.getElementById('root');
     const h1 = document.createElement('h1');
+    const h2 = document.createElement('h2');
+    
     h1.textContent = "Bienvenido a SPA";
+    h2.textContent = 'single page aplication';
+    
     root.appendChild(h1);
+    root.appendChild(h2);
 }
